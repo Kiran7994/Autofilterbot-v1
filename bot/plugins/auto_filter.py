479079bdd07b0c94bdd40b5f7cd8ec6320ea6231
Film gallery <88760900+Kiran7994@users.noreply.github.com>
2021-08-16 15:00:24 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -149,7 +149,7 @@ async def auto_filter(bot: Client, update: Message):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏩️", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("🅽🅴🆇🆃⏩️", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         

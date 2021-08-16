10e57ae2f142fe2b34934bc9fa549d96e0bac798
Film gallery <88760900+Kiran7994@users.noreply.github.com>
2021-08-16 14:29:51 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -149,13 +149,13 @@ async def auto_filter(bot: Client, update: Message):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏭️", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("Next ⏩️", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"⭕ Page 1/{len_result if len_result < max_pages else max_pages} ⭕", callback_data="ignore")
+            InlineKeyboardButton(f"🔷 Page 1/{len_result if len_result < max_pages else max_pages} 🔷", callback_data="ignore")
         ])
         
         
